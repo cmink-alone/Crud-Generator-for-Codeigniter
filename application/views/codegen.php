e534651b425bcf7fb3ef7b380f335c76f9778334
lmadhuranga <lilan.maduranga@gmail.com>
2014-05-10 23:46:06 +0530

    added primary key to hidden field and and id set

--- application/views/codegen.php
+++ application/views/codegen.php
@@ -35,6 +35,7 @@ if(isset($alias))
     $type = array(
                 'exclude'  =>'Do not include',
                 'text'     => 'text input',
+                'number'  => 'Numbers only',
                 'date'     => 'date input',
                 'email'    => 'email input',
                 'password' => 'password',
@@ -47,26 +48,35 @@ if(isset($alias))
 
         foreach($alias as $key => $a)
         {
-            // var_dump($a);
-            // exit();
             $email_default = FALSE;
             echo '<p> Field: '.$a->Field.'<br>Label:'.form_input('field['.$a->Field.']', str_replace("_"," ",ucfirst($a->Field))).' '.$a->Type;
             
+            echo ' max :'.form_input('max_length['.$a->Field.'][]',(empty($alias_2[$key]->CHARACTER_MAXIMUM_LENGTH)&&($alias_2[$key]->DATA_TYPE=='int'))? 11 :  (!is_null($alias_2[$key]->CHARACTER_MAXIMUM_LENGTH)? $alias_2[$key]->CHARACTER_MAXIMUM_LENGTH:'')) ;
            
             if(strpos($a->Type,'enum') !== false)
             {
                 echo ' <br>Enum Values (CSV): <input size="50" type="text" value="'.htmlspecialchars ("'0'=>'Value','1'=>'Another Value'").'" name="'.$a->Field.'default">';
                 $sel = 'dropdown';
             }
+            elseif($a->Key == 'PRI')
+            {
+                // exit('hi');
+                $sel = 'exclude';
+                echo form_hidden('primaryKey',$a->Field);
+            }
+            elseif(strpos($a->Type,'int') !== false)
+            {
+                $sel = 'number';
+            }
             elseif(strpos($a->Type,'blob') !== false || strpos($a->Type,'text') !== false)
             {
                 $sel = 'textarea';
             }
-            else if($a->Key == 'PRI')
-            {
-                $sel = 'exclude';
-                echo form_hidden('primaryKey',$a->Field);
-            }
+            // else if($a->Key == 'PRI')
+            // {
+            //     $sel = 'exclude';
+            //     echo form_hidden('primaryKey',$a->Field);
+            // }
             // else if(in_array('password',explode("_",$a->Field)))
             else if(strpos($a->Field,'password') !== false)
             {
@@ -107,6 +117,7 @@ if(isset($alias))
             // }
 
 
+            // echo ' max :'.form_input('rules['.$a->Field.'][]',(empty($alias_2[$key]->CHARACTER_MAXIMUM_LENGTH)&&($alias_2[$key]->DATA_TYPE=='int'))? 11 :  (!is_null($alias_2[$key]->CHARACTER_MAXIMUM_LENGTH)? $alias_2[$key]->CHARACTER_MAXIMUM_LENGTH:'')) ;
 
             // var_dump($alias_2[0]);
             // exit();
@@ -117,13 +128,13 @@ if(isset($alias))
                 // echo ' DATA_TYPE:'.form_input('DATA_TYPE['.$a->Field.']',$alias_2[$key]->DATA_TYPE);
             // }
             // echo ' max :'.form_input('rules['.$a->Field.'][]',(empty($alias_2[$key]->CHARACTER_MAXIMUM_LENGTH)&&($alias_2[$key]->DATA_TYPE=='int'))? 'max_length[11]' : !empty($alias_2[$key]->CHARACTER_MAXIMUM_LENGTH)?('max_length['.$alias_2[$key]->CHARACTER_MAXIMUM_LENGTH.']') : '' );
-            echo ' max :'.form_input('rules['.$a->Field.'][]',(empty($alias_2[$key]->CHARACTER_MAXIMUM_LENGTH)&&($alias_2[$key]->DATA_TYPE=='int'))? 'max_length[11]' :  (!is_null($alias_2[$key]->CHARACTER_MAXIMUM_LENGTH)?'max_length['.$alias_2[$key]->CHARACTER_MAXIMUM_LENGTH.']':'')) ;
             echo '<br>';
             echo form_checkbox('rules['.$a->Field.'][]', 'required', ((strtolower($a->Null)=='no') ? TRUE : FALSE) ) . ' required :: ';
             echo form_checkbox('rules['.$a->Field.'][]', 'trim', TRUE) . ' trim :: ';
             echo form_checkbox('rules['.$a->Field.'][]', 'valid_email', $email_default) . ' email :: ';
             echo form_checkbox('rules['.$a->Field.'][]', 'integer', (($alias_2[$key]->DATA_TYPE=='int') ? TRUE : FALSE)) . 'integer ::';
             echo form_checkbox('rules['.$a->Field.'][]', 'xss_clean', TRUE) . 'xss_clean ::';
+            echo ' max :'.form_hidden('rules['.$a->Field.'][]',(empty($alias_2[$key]->CHARACTER_MAXIMUM_LENGTH)&&($alias_2[$key]->DATA_TYPE=='int'))? 'max_length[11]' :  (!is_null($alias_2[$key]->CHARACTER_MAXIMUM_LENGTH)?'max_length['.$alias_2[$key]->CHARACTER_MAXIMUM_LENGTH.']':'')) ;
             //echo ':: custom rule '. form_input('rules['.$a->Field.'][]', '');
             echo '</p>';
             
