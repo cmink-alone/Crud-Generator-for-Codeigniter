ef2a8cf0521da2791c7c8324c3da93463238ace7
lmadhuranga <lilan.maduranga@gmail.com>
2014-05-10 23:43:34 +0530

    modal footer issue fixed

--- templates/view.php
+++ templates/view.php
@@ -5,14 +5,14 @@
 	<table class="table ">
 		{ind_view_list}
 	</table>
-	<div class="modal-footer" class="clearfix">
-		<!-- left footer -->
-		<div class="pull-left">
-			<?php echo anchor((site_url('{controller_name_l}'.'/edit/'.$result->{primaryKey_id}))  , "Edit", array('class'=>'btn btn-primary')); ?>
-			<?php echo anchor((site_url('{controller_name_l}')) , '{C_controller_name_l} list', array('class'=>'btn btn-default')); ?>
-		</div>
-		<!-- right footer  -->
-		<div class="span4 pull-right"><?php echo anchor(site_url(), ' &copy; '. date('Y').' '.$site_name); ?></div>
-	</div>
 	
+</div>
+<div class="modal-footer" class="clearfix">
+	<!-- left footer -->
+	<div class="pull-left">
+		<?php echo anchor((site_url('{controller_name_l}'.'/edit/'.$result->{primaryKey_id}))  , "Edit", array('class'=>'btn btn-primary')); ?>
+		<?php echo anchor((site_url('{controller_name_l}')) , '{C_controller_name_l} list', array('class'=>'btn btn-default')); ?>
+	</div>
+	<!-- right footer  -->
+	<div class="span4 pull-right"><?php echo anchor(site_url(), ' &copy; '. date('Y').' '.$site_name); ?></div>
 </div>
